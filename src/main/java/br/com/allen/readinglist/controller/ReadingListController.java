b8ee0b6fb66254421021152fef97080781d2a54d
AllenLVieira <allenvieira96@gmail.com>
2021-09-18 11:34:28 -0300

    [feat] Implementar a validação dos campos
    - Validação com uso de RegEx para o código ISBN do livro.
    - Validação para todos os campos com NotBlank

--- src/main/java/br/com/allen/readinglist/controller/ReadingListController.java
+++ src/main/java/br/com/allen/readinglist/controller/ReadingListController.java
@@ -5,8 +5,10 @@ import br.com.allen.readinglist.repository.ReadingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -31,7 +33,11 @@ public class ReadingListController {
     }
 
     @PostMapping("readinglist/{reader}")
-    public String addToReadingList(@PathVariable("reader") String reader, Book book){
+    public String addToReadingList(@PathVariable("reader") String reader, @Valid Book book, BindingResult result){
+        if(result.hasErrors()){
+            System.out.println(result);
+            return "readinglist";
+        }
         book.setReader(reader);
         readingListRepository.save(book);
         return "redirect:/readinglist/{reader}";
--- src/main/java/br/com/allen/readinglist/model/Book.java
+++ src/main/java/br/com/allen/readinglist/model/Book.java
@@ -1,29 +1,40 @@
 package br.com.allen.readinglist.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "The reader of the book must be identified.")
     private String reader;
+
+    @NotBlank(message = "The ISBN of the book must be identified.")
+    @Pattern(message = "ISBN is invalid.", regexp = "^(?:ISBN(?:-13)?:?\\ )?(?=[0-9]{13}$|(?=(?:[0-9]+[-\\ ]){4})[-\\ 0-9]{17}$)97[89][-\\ ]?[0-9]{1,5}[-\\ ]?[0-9]+[-\\ ]?[0-9]+[-\\ ]?[0-9]$")
     private String isbn;
+
+    @NotBlank(message = "The title of the book must be identified.")
     private String title;
+
+    @NotBlank(message = "The author of the book must be identified.")
     private String author;
+
+    @NotBlank(message = "The book description must be identified.")
     private String description;
 
     public Book(String reader, String isbn, String title, String author, String description) {
